e3eb8329940d646aac6ea4aab28e92da1ab970b1
l02 <isrLamb@outlook.com>
2022-04-08 18:26:41 +0300

    refactored unit tests for XmlParams

--- spec/unit/xml_params_spec.rb
+++ spec/unit/xml_params_spec.rb
@@ -3,11 +3,28 @@
 describe XmlParams do
   source_filepath = './tmp/86476809/(NS)-CHAUZIMU-MWA-CHILENGEDWE.doc'
   converted_filepath = './tmp/331411115.docx'
+  fonts_path = './assets/fonts'
   format = :docx
-  it 'Comparison of xml files' do
-    xml = described_class.new(fonts_path: './assets/fonts', tmp_path: StaticData::TMP_DIR)
-    xml_template = File.open('./spec/unit/template.xml') { |f| Nokogiri::XML(f) }
-    created_xml = File.open(xml.create_xml(source_filepath, converted_filepath, format)) { |f| Nokogiri::XML(f) }
+  xml = described_class.new(fonts_path: fonts_path, tmp_path: StaticData::TMP_DIR)
+  xml_template = File.open('./spec/unit/template.xml') { |f| Nokogiri::XML(f) }
+  created_xml = File.open(xml.create_xml(source_filepath, converted_filepath, format)) { |f| Nokogiri::XML(f) }
+  it 'check source filepath' do
+    expect(created_xml.at('m_sFileFrom').content).to eq(source_filepath)
+  end
+
+  it 'check converted filepath' do
+    expect(created_xml.at('m_sFileTo').content).to eq(converted_filepath)
+  end
+
+  it 'check conversion format' do
+    expect(created_xml.at('m_nFormatTo').content).to eq('65')
+  end
+
+  it 'check fonts path' do
+    expect(created_xml.at('m_sFontDir').content).to eq(fonts_path)
+  end
+
+  it 'comparison of xml files' do
     expect(created_xml.to_xml).to eq(xml_template.to_xml)
   end
 end
