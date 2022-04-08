298196581f5b198ade816655cb24931bf3a47130
l02 <isrLamb@outlook.com>
2022-04-08 19:13:51 +0300

    Corrections on Review Comments

--- spec/unit/xml_params_spec.rb
+++ spec/unit/xml_params_spec.rb
@@ -1,30 +1,30 @@
 # frozen_string_literal: true
 
 describe XmlParams do
-  source_filepath = './tmp/86476809/(NS)-CHAUZIMU-MWA-CHILENGEDWE.doc'
-  converted_filepath = './tmp/331411115.docx'
+  source_filepath = './tmp/file.doc'
+  converted_filepath = './tmp/file.docx'
   fonts_path = './assets/fonts'
   format = :docx
   xml = described_class.new(fonts_path: fonts_path, tmp_path: StaticData::TMP_DIR)
-  xml_template = File.open('./spec/unit/template.xml') { |f| Nokogiri::XML(f) }
-  created_xml = File.open(xml.create_xml(source_filepath, converted_filepath, format)) { |f| Nokogiri::XML(f) }
+  created_xml = xml.create_xml(source_filepath, converted_filepath, format)
+  parsed_result = File.open(created_xml) { |f| Nokogiri::XML(f) }
   it 'check source filepath' do
-    expect(created_xml.at('m_sFileFrom').content).to eq(source_filepath)
+    expect(parsed_result.at('m_sFileFrom').content).to eq(source_filepath)
   end
 
   it 'check converted filepath' do
-    expect(created_xml.at('m_sFileTo').content).to eq(converted_filepath)
+    expect(parsed_result.at('m_sFileTo').content).to eq(converted_filepath)
   end
 
   it 'check conversion format' do
-    expect(created_xml.at('m_nFormatTo').content).to eq('65')
+    expect(parsed_result.at('m_nFormatTo').content).to eq(XmlParams::FORMAT_NUMBERS[format])
   end
 
   it 'check fonts path' do
-    expect(created_xml.at('m_sFontDir').content).to eq(fonts_path)
+    expect(parsed_result.at('m_sFontDir').content).to eq(fonts_path)
   end
 
   it 'comparison of xml files' do
-    expect(created_xml.to_xml).to eq(xml_template.to_xml)
+    expect(File.read(created_xml)).to eq(File.read('./spec/unit/simple_sample.xml'))
   end
 end
