8d57839fa39b5e780e09c6bd2f9f105489c0bf67
MaxSorokin <askonev007@gmail.com>
2022-11-01 20:41:13 +0400

    Added OFORM_TO_ALL convert specs

---
+++ spec/functional/documents/oform/oform_to_txt_functional_spec.rb
@@ -0,0 +1,30 @@
+# frozen_string_literal: true
+
+require 'rspec'
+# palladium = PalladiumHelper.new(x2t.version, 'Oform to Txt')
+# result_sets = palladium.get_result_sets(StaticData::POSITIVE_STATUSES)
+# files = s3.files_from_folder('oform')
+
+format = 'oform'
+files = Dir["#{StaticData::TMP_DIR}/../documents/*.#{format}"]
+
+describe 'Conversion oform files to txt' do
+  before do
+    @tmp_dir = create_tmp_dir.first
+  end
+
+  # (files - result_sets.map { |result_set| "oform/#{result_set}" }).each do |file|
+  files.each do |file|
+    it File.basename(file) do
+      # s3.download_file_by_name(file, @tmp_dir)
+      FileUtils.cp(file, "#{@tmp_dir}/#{File.basename(file)}")
+      @file_data = x2t.convert("#{@tmp_dir}/#{File.basename(file)}", :txt)
+      expect(File).to exist(@file_data[:tmp_filename])
+    end
+  end
+
+  after do |example|
+    spec_cleanup(@tmp_dir, @file_data[:tmp_filename])
+    # palladium.add_result(example, @file_data)
+  end
+end
